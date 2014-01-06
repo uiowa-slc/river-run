33b503c91e31de1bbdca7ce2e61440c3a80a1d7a
Dustin Quam <quamsta@gmail.com>
2014-01-06 13:29:17 -0600

    re-added _config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,26 +3,22 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	"type" => 'MySQLDatabase',
-	"server" => 'localhost',
-	"username" => 'root',
-	"password" => 'root',
-	"database" => 'riverrun_dev',
-	"path" => '',
-);
+global $database;
+$database = 'riverrun';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
 
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.org/themes/
 SSViewer::set_theme('blackcandy');
-
 // Set the site locale
 i18n::set_locale('en_US');
 
 // enable nested URLs for this site (e.g. page/sub-page/)
 SiteTree::enable_nested_urls();
-Security::setDefaultAdmin("admin","nimlok22");
-Director::set_environment_type("dev");
+/* Paste as Plain Text By Default */
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky',true);
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default',true);
